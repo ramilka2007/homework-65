41a9ef27fee150e11a3b7e11e7955740597e7095
ramilka <lilya1976.03.03@gmail.com>
2024-07-05 05:22:42 +0600

    Init App

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis, quaerat.
+    </>
+  )
+};
+
+export default App
